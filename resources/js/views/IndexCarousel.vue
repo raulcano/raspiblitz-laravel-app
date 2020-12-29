cb5039c2ced04db65220d7f199c884dfed60212a
Raúl Cano <raul.cano.argamasilla@gmail.com>
2020-12-29 20:32:21 -0300

    remove margins

--- resources/js/views/IndexCarousel.vue
+++ resources/js/views/IndexCarousel.vue
@@ -2,14 +2,14 @@
   <div class="wrapper">
     <div class="md-layout">
         <div class="md-layout-item md-size-66 mx-auto md-small-size-100">
-          <md-card>
+          <md-card class="mt-0 mb-0 ml-0">
             <carousel
               :per-page="1"
               loop
               :speed="700"
               autoplay
               :autoplay-timeout="5000"
-              :mouse-drag="false"
+              :mouse-drag="true"
               navigationEnabled
               navigationNextLabel="<i class='material-icons'>keyboard_arrow_right</i>"
               navigationPrevLabel="<i class='material-icons'>keyboard_arrow_left</i>"
@@ -36,24 +36,6 @@
                 </div>
                 <img :src="bitcoin" alt="bitcoin" />
               </slide>
-              <slide>
-                <div class="carousel-caption">
-                  <h4>
-                    <md-icon>location_on</md-icon>
-                    Yellowstone National Park, United States
-                  </h4>
-                </div>
-                <img :src="carousel2" alt="carousel2" />
-              </slide>
-              <slide>
-                <div class="carousel-caption">
-                  <h4>
-                    <md-icon>location_on</md-icon>
-                    Yellowstone National Park, United States
-                  </h4>
-                </div>
-                <img :src="carousel3" alt="carousel3" />
-              </slide>
             </carousel>
           </md-card>
         </div>
@@ -73,9 +55,6 @@ export default {
   },
   data() {
     return {
-      carousel1: require("../assets/img/nature-2.jpg"),
-      carousel2: require("../assets/img/nature.jpg"),
-      carousel3: require("../assets/img/nature-3.jpg"),
       bitcoin: require("../assets/img/bitcoin-background.webp"),
       status: require("../assets/img/raspiblitz-status.png")
     };
@@ -97,5 +76,7 @@ export default {
   .btn-container {
     display: flex;
   }
+
 }
+
 </style>
